f7508b35892481f168cd345b39f8908c8269ba03
R0CKK <102409405+R0CKK@users.noreply.github.com>
2024-09-23 15:37:31 +0530

    Create create_db.py

---
+++ create_db.py
@@ -0,0 +1,19 @@
+import mysql
+CREATE TABLE IF NOT EXISTS stock (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  symbol VARCHAR(20) NOT NULL UNIQUE,
+  company VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS stock_price (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  stock_id INT,
+  date DATE NOT NULL,
+  open DECIMAL(10, 2) NOT NULL,
+  high DECIMAL(10, 2) NOT NULL,
+  low DECIMAL(10, 2) NOT NULL,
+  close DECIMAL(10, 2) NOT NULL,
+  adjusted_close DECIMAL(10, 2) NOT NULL,
+  volume INT NOT NULL,
+  FOREIGN KEY (stock_id) REFERENCES stock (id)
+);
